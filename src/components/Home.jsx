0f5bd90094e5e09181373d73129b0511034adfad
sheakabhi25 <ag893573@gmail.com>
2023-04-16 00:16:28 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,97 @@
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/6.jpg';
+import img2 from '../assets/7.jpg';
+import img3 from '../assets/9.jpeg';
+import img4 from '../assets/10.jpeg';
+import img5 from '../assets/11.webp';
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
+};
+
+const Home = () => {
+  return (
+    <Box>
+      <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m="auto"
+        >
+          Memories
+        </Heading>
+
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+            Memories are an integral part of our lives, shaping who we are and how we interact with the world around us. Good memories bring joy, happiness and a sense of nostalgia, while bad memories can cause pain, sorrow and even trauma. Memories are responsible for our perceptions of the past, our understanding of the present and our hopes for the future. They can inspire us, drive us and sometimes haunt us. In the end, memories are what make us human and give our lives meaning
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={1000}
+    showStatus={false}
+    showThumbs={false}
+    showArrows={false}
+  >
+    <Box w="full" h={'100vh'}>
+      <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+      Time travel gallery
+      </Heading>
+    </Box>
+    <Box w="full" h={'100vh'}>
+      <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+      Memories come alive
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+      Time travel gallery.
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Night life is cool
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
